d617d6be60537658979d601010754635134c959b
Rock2638 <162598415+Rock2638@users.noreply.github.com>
2024-03-29 14:09:33 +0800

    added some stuff

--- myfile_1.py
+++ myfile_1.py
@@ -1,2 +1,12 @@
 print("hello david")
 print ("maler sa dil")
+
+for x in range(10):
+    print (x)   
+
+#answer = Y/N
+answer = input("do you want to contintue? [Y/N]")
+
+if answer.upper() =="Y":
+    for x in range(1, 11):
+        print(x)
